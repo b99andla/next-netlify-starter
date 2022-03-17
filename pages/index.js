321e3049f38b82a88fde091be1cf35317f88311b
Andreas Lans <al@mindius.se>
2022-03-17 14:11:52 +0100

    Remove md

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
 import { Component } from 'react';
-import { attributes, react as HomeContent } from '../content/home.md';
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
     return (
       <>
         <Head>
@@ -12,16 +10,7 @@ export default class Home extends Component {
           <link rel="stylesheet" href="style.css" />
         </Head>
         <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
+          <h1>Test</h1>
         </article>
       </>
     )
